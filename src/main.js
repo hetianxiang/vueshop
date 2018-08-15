b8afa59576d54ccd490cc515dfebba1d9003cb9c
tianxiang <rogermhe@gmail.com>
2018-08-15 11:01:18 +0800

    添加babel-plugin-import，实现按需 引入

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vant from 'vant'
-import 'vant/lib/vant-css/index.css'
+import {
+  Button
+} from 'vant'
+// import 'vant/lib/vant-css/index.css'
 
-Vue.use(Vant)
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
@@ -14,6 +16,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
